37ea858724af1a540866167a30e30a2d62992d6d
bliti <pablo.rivera.programmer@gmail.com>
2014-12-20 12:50:15 -0400

    es sabado

--- pagina/urls.py
+++ pagina/urls.py
@@ -1,6 +1,8 @@
-from .views import InicioView
+from django.conf.urls import url
+from .views import InicioView, EntrarView
 
 
-urlpatterns = patterns('',
-    (r'^$', InicioView.as_view(), name="inicio")
-)
+urlpatterns = (
+    url(r'^$', InicioView.as_view(), name='inicio'),
+    url(r'^/entrar$', EntrarView.as_view(), name='entrar'),
+    )
--- pagina/views.py
+++ pagina/views.py
@@ -1,5 +1,21 @@
 from django.views.generic import TemplateView
+from django.shortcuts import redirect, render
+from django.core.urlresolvers import reverse_lazy
 
 
 class InicioView(TemplateView):
-    template_name = 'inicio.html'
+
+
+    def get(self, request, *args, **kwargs):
+        """
+        check if user is logged in to redirect to 
+        dashboard
+        """
+        if request.user.is_authenticated():
+            return redirect(reverse_lazy('entrar'))
+        
+        return render(self.request, 'inicio.html')
+
+
+class EntrarView(TemplateView):
+    template_name = 'entrar.html'
