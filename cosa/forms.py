715b2b5aaaad703b607b5b792d44c690350f6a62
bliti <pablo.rivera.programmer@gmail.com>
2015-01-02 16:03:27 -0400

    wepele

--- cosa/forms.py
+++ cosa/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 
 class CrearCosaForm(forms.Form):
+    
+    
+    cantidad = forms.IntegerField(label="Cantidad", attrs={})
+    categoria = forms.ModelChoiceField(queryset=, label=, to_field_name="categoria", attrs={})
+    comentario = forms.CharField(label="Comentario", attrs={})
