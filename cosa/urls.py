dc5eece2ef89eba21599155b2fd5311e9d16bfd6
bliti <pablo.rivera.programmer@gmail.com>
2015-01-07 01:45:05 -0400

    pagination done, needs styling

--- cosa/urls.py
+++ cosa/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from pagina.views import DeleteCosaView
+from .views import CosaListView
+from pagina.views import DeleteCosaView #Note: bring this class over to this file!
 
 
 urlpatterns = (
+    url(r'^$', login_required(CosaListView.as_view()), name='cosas'),
     url(r'^delete/$', login_required(DeleteCosaView.as_view()), name='delete_cosa'),
     )
